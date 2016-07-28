e2818d1d3adfa58784254d4dff4622abeeaef2ca
Dapeng <verydapeng@gmail.com>
2016-07-28 15:27:04 +0800

    init

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String hello() {
+        return "hello " + new Date();
+    }
+}
